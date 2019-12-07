98b9be86ed6d503802224d8b53723e00b336b54f
dissi <moreiras@informatik.uni-freiburg.de>
2019-12-07 19:37:03 +0100

    first basic CLI

---
+++ sonar_telegram.py
@@ -0,0 +1,158 @@
+import os
+import sys
+import asyncio
+import json
+import datetime
+from telethon import TelegramClient
+from telethon.tl.types import DocumentAttributeFilename
+from tika import parser
+from sonarclient import SonarClient
+from telegram_api_credentials import api_id, api_hash
+
+
+class SonarTelegram():
+
+    def __init__(self, loop, api_id, api_hash, island, session_name, endpoint):
+        self.loop = loop or asyncio.get_event_loop()
+        self.sonar = SonarClient(endpoint, island)
+        self.api_id = api_id
+        self.api_hash = api_hash
+        self.telegram = TelegramClient(session_name,
+                                       self.api_id,
+                                       self.api_hash,
+                                       loop=self.loop)
+        self.data = {}
+
+    async def ensure_schema(self, schema_name, schema):
+        schema = await self.sonar.get_schema(schema_name)
+        if schema:
+            return schema
+        elif await self.sonar.set_schema():
+            return True
+        else:
+            return False
+
+    async def get_jsondialogs(self):
+        dialogs_list = []
+        dialogs = self.telegram.iter_dialogs()
+        async for dialog in dialogs:
+            dialog_json = await self.create_dialog_schema(dialog)
+            dialogs_list.append(dialog_json)
+        return dialogs_list
+
+    async def create_dialog_schema(self, dialog):
+        return json.dumps({
+            "name": dialog.name,
+            "date": dialog.date.isoformat(),
+            "last_message": dialog.message.message,
+            "dialog_id": dialog.id,
+        })
+
+    async def get_messages(self, dialog_id):
+        msg_set = []
+        msgs = self.telegram.iter_messages(dialog_id)
+        async for msg in msgs:
+            print(json.dumps(msg.to_dict(), default=date_format))
+            msg_json = await self.create_message_schema(msg)
+            msg_set.append(msg_json)
+        return msg_set
+
+    async def create_message_schema(self, message):
+        return json.dumps({
+            "id": message.id,
+            "from_id": message.from_id,
+            "date": message.date.isoformat(),
+            "content": message.message
+        })
+
+    def create_webpage_schema(self, webpage, msg_id):
+        return json.dumps({
+            "title": webpage.title,
+            "url": webpage.url,
+            "sitename": webpage.site_name,
+            "description": webpage.description,
+            "content_type": "website",
+            "telegram_id": msg_id
+        })
+
+    async def create_document_json(self, document, datadir, msg_id):
+        for attribute in document.attributes:
+            if type(attribute) is DocumentAttributeFilename:
+                print(attribute)
+                if not os.path.isfile(datadir + "/" + attribute.file_name):
+                    path = await self.telegram.download_media(
+                        message=document,
+                        file=datadir
+                    )
+
+                    return {
+                        "filename": attribute.file_name,
+                        "path": path,
+                        "extracted_text": parser.from_file(
+                            path
+                        )["content"],
+                        "telegram_id": msg_id,
+                        "content_type": "document",
+                        "mime_type": document.mime_type
+                    }
+
+
+async def demo(client):
+    dialogs = await client.get_jsondialogs()
+    last_dialog_id = json.loads(dialogs[0])["dialog_id"]
+    messages = await client.get_messages(last_dialog_id)
+    for msg in messages:
+        print("#"*10)
+        msg = json.loads(msg)
+        print(msg)
+        id = await client.sonar.put({
+            "schema": "telegram.message",
+            "value": msg,
+            "id": "telegram." + str(msg["id"])
+        })
+        print("imported: " + id)
+
+
+def date_format(message):
+    if type(message) == datetime:
+        return message.isoformat()
+
+
+async def init(loop, callback=None, opts=None):
+    print("INIT")
+    client = SonarTelegram(
+        loop=loop,
+        api_id=api_id,
+        api_hash=api_hash,
+        session_name='anon',
+        island='default',
+        endpoint='http://localhost:9191/api')
+
+    try:
+        await client.telegram.connect()
+        await client.telegram.start()
+    except Exception as e:
+        print('Failed to connect', e, file=sys.stderr)
+        return
+
+    if callback:
+        async with client.telegram:
+            try:
+                await callback(client, opts)
+                await client.sonar.close()
+            except Exception as e:
+                print('Failed in command callback', e, file=sys.stderr)
+                return
+
+        # async with client.telegram:
+            # await client.telegram.run_until_disconnected()
+
+    return loop
+
+if __name__ == "__main__":
+    aio_loop = asyncio.get_event_loop()
+    try:
+        aio_loop.run_until_complete(init(aio_loop))
+    finally:
+        if not aio_loop.is_closed():
+            aio_loop.close()
---
+++ sonar_telegram_cli.py
@@ -0,0 +1,67 @@
+from sonar_telegram import init
+import asyncio
+import click
+from telethon import events
+import logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def dialogs():
+    loop(dialogs_cb)
+
+
+@cli.command()
+@click.option('-d', '--dialog_id', default='all')
+def listen(dialog_id):
+    if dialog_id == 'all':
+        opts = {}
+    else:
+        opts = {"dialog_id": int(dialog_id)}
+    loop(listen_cb, opts)
+
+
+@cli.command()
+@click.argument('dialog_id')
+def dialog(dialog_id):
+    opts = {"dialog_id": int(dialog_id)}
+    loop(get_dialog_cb, opts)
+
+
+async def listen_cb(client, opts={}):
+    @client.telegram.on(events.NewMessage(chats=(opts.get('dialog_id'))))
+    async def listen(event):
+        print('{}'.format(event.message))
+
+    # listen for events until the connection is closed (ie forever)
+    async with client.telegram:
+        await client.telegram.run_until_disconnected()
+
+
+async def get_dialog_cb(client, opts={}):
+    dialog_id = opts.get("dialog_id")
+    dialog = await client.get_messages(dialog_id)
+    print(dialog)
+    # print("Name: {}, User_ID: {}, Dialog_ID: {}  ".format(dialog.name, dialog))
+    return dialog
+
+
+async def dialogs_cb(client, opts={}):
+    dialogs = await client.get_jsondialogs()
+    print(dialogs)
+    return dialogs
+
+
+def loop(callback, opts={}):
+    aio_loop = asyncio.get_event_loop()
+    try:
+        aio_loop.run_until_complete(init(aio_loop, callback, opts))
+    finally:
+        if not aio_loop.is_closed():
+            aio_loop.close()
